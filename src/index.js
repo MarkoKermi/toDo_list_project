947cd37d84cd18e6de1a8e3bb597d8ed38094ea5
Marko Kermiciev <kermicievmarko@gmail.com>
2023-01-18 01:51:01 +0100

    setting the JS part

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-import './style.css';
+import "./style.css";
+
+const tasks = [
+  {
+    description: "Learn JS, HTML & CSS",
+    completed: false,
+    index: 1,
+  },
+  {
+    description: "Go to gym",
+    completed: true,
+    index: 2,
+  },
+  {
+    description: "Buy protein",
+    completed: false,
+    index: 3,
+  },
+];
+
+const taskList = () => {
+  let content = "";
+  tasks.forEach((task) => {
+    content += `
+    <ul  id="task-list">
+        <li class="task-list" id="toDo"><input type="checkbox" id="checkbox"  value="task">${task.description}</li>
+    </ul>
+
+    `;
+  });
+  return content;
+};
+
+const rendering = () => document.getElementById("to-dos");
+
+window.addEventListener("load", () => {
+  const content = taskList();
+  rendering().innerHTML = content;
+});
