157f5ba1e21a4dccc050f5b549b4a5d012dff34f
Marko Kermiciev <kermicievmarko@gmail.com>
2023-01-19 13:28:28 +0100

    ESLint errors done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import "./style.css";
+import './style.css';
 
 const updateLocalStorage = (toDoListArray) => {
-  localStorage.setItem("toDoList", JSON.stringify(toDoListArray));
+  localStorage.setItem('toDoList', JSON.stringify(toDoListArray));
 };
 
 const getLocalStorage = () => {
-  const localStorageData = JSON.parse(localStorage.getItem("toDoList"));
+  const localStorageData = JSON.parse(localStorage.getItem('toDoList'));
   let toDoListArray;
   if (localStorageData) {
     toDoListArray = localStorageData;
@@ -16,38 +16,38 @@ const getLocalStorage = () => {
 };
 
 const toDoTasks = getLocalStorage();
-const toDoList = document.querySelector(".toDo_list_ul");
+const toDoList = document.querySelector('.toDo_list_ul');
 
 const addTask = (toDoListArray, task) => {
   toDoListArray.push({ task, id: toDoListArray.length + 1 });
 };
 
 const renderToDoList = (toDoListArray) => {
-  toDoList.innerHTML = "";
+  toDoList.innerHTML = '';
 
   toDoListArray = getLocalStorage();
 
   toDoListArray.forEach((toDo) => {
-    const toDoItem = document.createElement("li");
-    toDoItem.classList.add("todo-list-li");
+    const toDoItem = document.createElement('li');
+    toDoItem.classList.add('todo-list-li');
 
-    const toDoCheckbox = document.createElement("input");
-    toDoCheckbox.classList.add("todo-list-li-checkbox");
-    toDoCheckbox.type = "checkbox";
+    const toDoCheckbox = document.createElement('input');
+    toDoCheckbox.classList.add('todo-list-li-checkbox');
+    toDoCheckbox.type = 'checkbox';
     toDoItem.appendChild(toDoCheckbox);
 
-    const toDoText = document.createElement("input");
-    toDoText.classList.add("todo-list-li-text");
+    const toDoText = document.createElement('input');
+    toDoText.classList.add('todo-list-li-text');
     toDoText.value = toDo.task;
     toDoItem.appendChild(toDoText);
 
     if (toDo.completed) {
-      toDoText.classList.add("completed");
+      toDoText.classList.add('completed');
     }
 
-    const crossIcon = document.createElement("span");
-    crossIcon.classList.add("todo-list-li-cross");
-    crossIcon.innerHTML = "x";
+    const crossIcon = document.createElement('span');
+    crossIcon.classList.add('todo-list-li-cross');
+    crossIcon.innerHTML = 'x';
     toDoItem.appendChild(crossIcon);
 
     toDoList.appendChild(toDoItem);
@@ -55,10 +55,10 @@ const renderToDoList = (toDoListArray) => {
 };
 
 const deleteTask = (e, toDoListArray) => {
-  const clickedCross = e.target.closest(".todo-list-li-cross");
+  const clickedCross = e.target.closest('.todo-list-li-cross');
   const clickedTask = clickedCross.previousElementSibling;
   const taskIndex = toDoListArray.findIndex(
-    (task) => task.task === clickedTask.value
+    (task) => task.task === clickedTask.value,
   );
   toDoListArray.splice(taskIndex, 1);
   toDoListArray.forEach((task, index) => {
@@ -69,58 +69,53 @@ const deleteTask = (e, toDoListArray) => {
 };
 
 const markTask = (e, toDoListArray) => {
-  const clickedCheckbox = e.target.closest(".todo-list-li-checkbox");
+  const clickedCheckbox = e.target.closest('.todo-list-li-checkbox');
   const clickedTask = clickedCheckbox.nextElementSibling;
   const taskIndex = toDoListArray.findIndex(
-    (task) => task.task === clickedTask.value
+    (task) => task.task === clickedTask.value,
   );
   toDoListArray[taskIndex].completed = !toDoListArray[taskIndex].completed;
   updateLocalStorage(toDoListArray);
   renderToDoList(toDoListArray);
 };
 
-const input = document.querySelector(".task-input");
-const todoList = document.querySelector(".toDo_list_ul");
-const clearCompletedBtn = document.querySelector(".clearbtn");
-const addTaskBtn = document.querySelector(".add_btn");
+const input = document.querySelector('.task-input');
+const todoList = document.querySelector('.toDo_list_ul');
+const clearCompletedBtn = document.querySelector('.clearbtn');
+const addTaskBtn = document.querySelector('.add_btn');
 
-input.addEventListener("keypress", (e) => {
-  if (e.key === "Enter" && input.value !== "") {
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && input.value !== '') {
     addTask(toDoTasks, input.value);
-    input.value = "";
+    input.value = '';
     updateLocalStorage(toDoTasks);
     renderToDoList(toDoTasks);
   }
 });
 
-addTaskBtn.addEventListener("click", () => {
-  if (input.value !== "") {
+addTaskBtn.addEventListener('click', () => {
+  if (input.value !== '') {
     addTask(toDoTasks, input.value);
-    input.value = "";
+    input.value = '';
     updateLocalStorage(toDoTasks);
     renderToDoList(toDoTasks);
   }
 });
 
-todoList.addEventListener("click", (e) => {
-  if (e.target.closest(".todo-list-li-checkbox")) {
+todoList.addEventListener('click', (e) => {
+  if (e.target.closest('.todo-list-li-checkbox')) {
     markTask(e, toDoTasks);
   }
 });
 
-clearCompletedBtn.addEventListener("click", () => {
+clearCompletedBtn.addEventListener('click', () => {
   const toDoList = clearCompletedBtn(toDoTasks);
   updateLocalStorage(toDoList);
   renderToDoList(toDoList);
 });
 
-todoList.addEventListener("dblclick", (e) => {
-  if (e.target.closest(".todo-list-li-text")) {
-  }
-});
-
-todoList.addEventListener("click", (e) => {
-  if (e.target.closest(".todo-list-li-cross")) {
+todoList.addEventListener('click', (e) => {
+  if (e.target.closest('.todo-list-li-cross')) {
     deleteTask(e, toDoTasks);
   }
 });
