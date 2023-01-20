0819984e6a394e9de4e651cb9424cf77251d38ee
Marko Kermiciev <kermicievmarko@gmail.com>
2023-01-20 08:10:47 +0100

    eslint errors done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { renderToDoList } from "./modules/lists.js";
-import toDoTasks from "./tasks.js";
-import "./modules/eventListeners.js";
-import "./style.css";
+import { renderToDoList } from './modules/lists.js';
+import toDoTasks from './tasks.js';
+import './modules/eventListeners.js';
+import './style.css';
 
 renderToDoList(toDoTasks);
